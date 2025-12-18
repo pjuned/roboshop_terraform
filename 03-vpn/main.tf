c084a070e5279b8e2dbab4173bb6f8e8130f281b
unknown <pjuned.it@gmail.com>
2025-12-18 16:33:40 +0530

    OK

--- 03-vpn/main.tf
+++ 03-vpn/main.tf
@@ -1,32 +1,32 @@
-resource "aws_key_pair" "openvpn" {
-  key_name   = "openvpn"
-  public_key = file("C:\\Users\\junaid\\.ssh\\github2.pub") # for mac use /
-}
+  resource "aws_key_pair" "openvpn" {
+    key_name   = "openvpn"
+    public_key = file("C:\\Users\\junaid\\.ssh\\github2.pub") # for mac use /
+  }
 
-resource "aws_instance" "openvpn" {
-    ami = data.aws_ami.openvpn.id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
-    subnet_id = split("," , data.aws_ssm_parameter.public_subnet_ids.value)[0]
-    key_name = aws_key_pair.openvpn.key_name
-    user_data = file("openvpn.sh")
-    
-    tags = merge (
-        local.common_tags,
-        {
-            Name = "${var.project_name}-${var.environment}-openvpn"
-        }
-    )
-}
+  resource "aws_instance" "openvpn" {
+      ami = data.aws_ami.openvpn.id
+      instance_type = "t3.micro"
+      vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+      subnet_id = split("," , data.aws_ssm_parameter.public_subnet_ids.value)[0]
+      key_name = aws_key_pair.openvpn.key_name
+      user_data = file("openvpn.sh")
+      
+      tags = merge (
+          local.common_tags,
+          {
+              Name = "${var.project_name}-${var.environment}-openvpn"
+          }
+      )
+  }
 
-resource "aws_route53_record" "openvpn" {
-  zone_id = var.zone_id
-  name    = "openvpn.${var.domain_name}" # openvpn.daws86s.fun
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.openvpn.public_ip]
-  allow_overwrite = true
-}
+  resource "aws_route53_record" "openvpn" {
+    zone_id = var.zone_id
+    name    = "openvpn.${var.domain_name}" # openvpn.daws86s.fun  
+    type    = "A"
+    ttl     = 1
+    records = [aws_instance.openvpn.public_ip]
+    allow_overwrite = true
+  }
 
 # module "vpn" {
 #   source                 = "terraform-aws-modules/ec2-instance/aws"
